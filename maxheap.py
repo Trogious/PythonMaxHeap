f69c738ff167f8f8fcd12c54c993d471e9bff9e8
Trogious <trog@swmud.net>
2019-07-13 20:01:55 +0100

    next

--- maxheap.py
+++ maxheap.py
@@ -2,49 +2,76 @@
 class MaxHeap:
     def __init__(self):
         self.h = []
-        self.positions = {}
+        self._positions = {}
+        self._size = 0
+
+    def empty(self):
+        return len(self.h) < 1
 
     def push(self, item):
         self.h.append(item)
-        i = len(self.h)-1
+        self._size += 1
+        i = self._size-1
         while self.h[i] > self.h[MaxHeap._parent(i)]:
             self._swap(i, MaxHeap._parent(i))
             i = MaxHeap._parent(i)
-        self.positions[item] = i
+        self._positions[item] = i
 
-    def pop(self, item):
-        i = self.index(item)
-        if i < 0:
-            return
-        while not self._is_leaf(i):
-            
-        self.h.remove(len(self.h)-1)
+    def pop(self):
+        top = self.h[0]
+        self._remove(0)
+        return top
+
+    def remove(self, item):
+        self._remove(self._index(item))
+
+    def _remove(self, i):
+        item = self.h[i]
+        if i < len(self.h)-1:
+            self._swap(i, len(self.h)-1)
+            self.h.pop()
+            self._size -= 1
+            self._heapify(i)
+        else:
+            self.h.pop()
+            self._size -= 1
+        del self._positions[item]
 
     def peek(self):
         return self.h[0]
 
-    def index(self, item):
-        if item in self.positions:
-            return self.positions[item]
-        else:
-            return -1
+    def _index(self, item):
+        return self._positions[item]
 
     def _heapify(self, i):
         if not self._is_leaf(i):
-            if self.h[i] < self.h[MaxHeap._left(i)] or self.h[i] < self.h[MaxHeap._right(i)]:
-                if self.h[MaxHeap._left(i)] > self.h[MaxHeap._right(i)]:
-                    self._swap(i, MaxHeap._left(i))
-                    self._heapify(MaxHeap._left(i))
+            left = MaxHeap._left(i)
+            right = MaxHeap._right(i)
+            if left < self._size and self.h[i] < self.h[left]:
+                if right < self._size and self.h[i] < self.h[right]:
+                    if self.h[left] > self.h[right]:
+                        self._swap(i, left)
+                        self._heapify(left)
+                    else:
+                        self._swap(i, right)
+                        self._heapify(right)
                 else:
-                    self._swap(i, MaxHeap._right(i))
-                    self._heapify(MaxHeap._right(i))
+                    self._swap(i, left)
+                    self._heapify(left)
 
     def _swap(self, i, j):
+        self._positions[self.h[i]], self._positions[self.h[j]] = j, i
         self.h[i], self.h[j] = self.h[j], self.h[i]
 
     def _is_leaf(self, i):
         size = len(self.h)
         return i >= (size / 2) and i <= size
+
+    def _print_pos(self):
+        pos = ''
+        for k in self._positions.keys():
+            pos += 'h[%d]=%d ' % (self._positions[k], k)
+        return pos
 
     @staticmethod
     def _left(i):
@@ -59,9 +86,27 @@ class MaxHeap:
         return int((i-1)/2)
 
 
+def test_remove(mh, x):
+    import sys
+    sys.stdout.write('removing: %d, %s -> ' % (x, str(mh.h)))
+    # pos = mh._print_pos()
+    mh.remove(x)
+    print(mh.h)
+    # print(pos)
+
+
 mh = MaxHeap()
 for x in [30, 10, 5, 4, 3, 32, 21]:
     mh.push(x)
-print(mh.peek())
-print(mh.index(7))
-print(mh.h)
+test_remove(mh, 3)
+test_remove(mh, 32)
+test_remove(mh, 5)
+test_remove(mh, 4)
+test_remove(mh, 30)
+for x in [12, 43, 15]:
+    mh.push(x)
+test_remove(mh, 10)
+test_remove(mh, 21)
+test_remove(mh, 43)
+test_remove(mh, 15)
+test_remove(mh, 12)
